cce41e7e7ba09360801d8a272890a415d5b64227
Krista <krph6495@colorado.edu>
2023-09-17 02:28:54 +0000

    style fixes and minor changes

--- my_utils.py
+++ my_utils.py
@@ -1,11 +1,12 @@
 def get_column(file_name, query_column, query_value, result_column=3):
     result_list = []
-    
+
     with open(file_name, "r") as file:
-        
         for line in file:
             data = line.rstrip().split(',')
-            if data[query_column-1] == str(query_value): #change query_value to string to match data[index] type
-                float_convert = float((data[result_column-1])) #converts str to float first bec of decimal values
+            if data[query_column-1] == str(query_value):
+                # change query_value to string to match data[index] type
+                float_convert = float((data[result_column-1]))
+                # converts str to float first bec of decimal values
                 result_list.append(int(float_convert))
     return result_list
